40acfed5a6352506a7492839eba2c1e3b9af0f9b
Meldin Radoncic <meldin@macbook-pro.mynetworksettings.com>
2022-12-16 17:42:44 -0500

    added / route for the test

--- Routes/openAIroutes.js
+++ Routes/openAIroutes.js
@@ -1,7 +1,12 @@
+const { application } = require('express');
 const express = require('express');
 const { generateImage } = require('./controller/openAIcontroller');
 
 const openAIrouter = express.Router()
+
+openAIrouter.get('/', (req,res) => {
+    res.send('Its working')
+})
 
 openAIrouter.post('/generateimage', generateImage)
 
