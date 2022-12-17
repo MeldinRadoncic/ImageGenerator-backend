8cff7e0e70ebaf5271c22ba1e3158cf26ebb94bd
Meldin Radoncic <meldin@macbook-pro.mynetworksettings.com>
2022-12-16 19:53:22 -0500

    removing middleware

--- index.js
+++ index.js
@@ -4,13 +4,7 @@ const cors = require('cors')
 const openAIrouter = require('./Routes/openAIroutes')
 const app = express()
 
-app.use(express.json())
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-    next();
-}); 
+
 
 app.use(express.urlencoded({ extended:false }))
 app.use(cors())
