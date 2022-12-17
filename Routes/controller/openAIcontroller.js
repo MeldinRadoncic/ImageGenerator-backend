27102e5e14c5d20d4c819ebf039c81d92aa75b6a
Meldin Radoncic <meldin@macbook-pro.mynetworksettings.com>
2022-12-16 19:47:13 -0500

    logging errors and status codes

--- Routes/controller/openAIcontroller.js
+++ Routes/controller/openAIcontroller.js
@@ -20,8 +20,10 @@ const generateImage = async (req, res) => {
                 })
                 const imageUrl = response.data.data[0].url
                 res.status(200).json({ success:true, data:imageUrl })
+                console.log("This is server side status code " + res.status)
 
         } catch (error) {
+                console.log('This is server side error ' + error)
                 res.status(400)
                 res.json({message:"Sorry, That image cannot be generated. Please try again."})
         }
